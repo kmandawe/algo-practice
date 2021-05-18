07ca5d9da5df96d9eaf7b74d0cec6c6c587561ac
kmandawe <kdmandawe06@yahoo.com.ph>
2021-05-18 10:18:29 +0800

    In-order Traversal without recursion

---
+++ src/main/java/com/kensbunker/algo/InOrderTraversalWithoutRecursion.java
@@ -0,0 +1,39 @@
+package com.kensbunker.algo;
+
+import java.util.*;
+
+public class InOrderTraversalWithoutRecursion {
+
+    public List<Integer> inorderTraversal(TreeNode<Integer> root) {
+        List<Integer> finalList = new ArrayList<>();
+        Stack<TreeNode<Integer>> helperStack = new Stack<>();
+        if (root == null) {
+            return finalList;
+        }
+
+        TreeNode<Integer> current = root;
+        while (!helperStack.isEmpty() || current != null) {
+            while (current != null) {
+                helperStack.push(current);
+                current = current.left;
+            }
+
+            current = helperStack.pop();
+            finalList.add(current.val);
+
+            current = current.right;
+        }
+        return finalList;
+    }
+
+    public static void main(String[] args) {
+        MinHeightBstFromSortedArray bt = new MinHeightBstFromSortedArray();
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode<Integer> root = bt.buildDriver(input);
+
+        InOrderTraversalWithoutRecursion opt = new InOrderTraversalWithoutRecursion();
+        System.out.println(opt.inorderTraversal(root));
+
+
+    }
+}
